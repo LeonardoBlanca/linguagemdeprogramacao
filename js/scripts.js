88a61d5c74d02115f4e2e7f6da2663c64585f5a1
Leonardo Blanca <145368092+LeonardoBlanca@users.noreply.github.com>
2025-02-25 09:03:58 -0300

    Add Exercise 01

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 function seguir(){
     alert("Você agora está seguindo Leonardo Silva")
 }
+
+function exe01(){
+    // Vamos usar DOM para recuperar os dados do HTML
+    // DOM - Document Object Model
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+
+    // Vamos fazer a subtração
+    let sub = nro1 - nro2
+
+    // Mostrar o resultado
+    // alert("A subração é: " + sub)
+    document.getElementById("sub").innerText = "O resultdo é: " + sub
+}
