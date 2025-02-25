4d0310cf655ea53b90e4bef7ddf14f1fc4952942
Leonardo Blanca <145368092+LeonardoBlanca@users.noreply.github.com>
2025-02-25 09:23:01 -0300

    Add Exercicio 02

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,14 @@ function exe01(){
     // alert("A subração é: " + sub)
     document.getElementById("sub").innerText = "O resultdo é: " + sub
 }
+
+function exe02(){
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    let nro3 = Number(document.getElementById("nro3").value)
+
+    // Multiplicando
+    let result = nro1 * nro2 * nro3
+
+    document.getElementById("result").innerHTML = "O resultado é: " + result
+}
